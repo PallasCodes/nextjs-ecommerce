6f599274a4f4ed9505e4fcaccda4712ddb143665
pallascodes <pallascodes@gmail.com>
2025-06-26 17:49:44 -0600

    feat: add Sidebar component and integrate into layout

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,10 +1,11 @@
-import { TopMenu } from '@/components'
+import { Sidebar, TopMenu } from '@/components'
 import { PropsWithChildren } from 'react'
 
 export default function layout({ children }: PropsWithChildren) {
   return (
     <main className="min-h-screen">
       <TopMenu />
+      <Sidebar />
       <div className="px-0 sm:px-10">{children}</div>
     </main>
   )
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './ui/not-found/PageNotFound'
+export * from './ui/sidebar/Sidebar'
 export * from './ui/title/Title'
 export * from './ui/top-menu/TopMenu'
 
