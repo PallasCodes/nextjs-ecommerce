2653d4fea57186e4b9da692a09db5e4f9314afca
pallascodes <pallascodes@gmail.com>
2025-07-28 13:33:47 -0600

    feat: database seeds created for product, productImage and category

---
+++ src/lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '../generated/prisma'
+
+const prismaclientSingleton = () => {
+  return new PrismaClient()
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaclientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? prismaclientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
